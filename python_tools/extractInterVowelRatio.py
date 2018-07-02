02b2c9e1890ac30b67c319046877c94cfbe16425
Kithao <quy-thao.truong@eleves.ec-nantes.fr>
2018-07-02 22:32:22 +0900

    add ratio calculation of consecutive syllables

--- python_tools/extractInterVowelRatio.py
+++ python_tools/extractInterVowelRatio.py
@@ -45,13 +45,39 @@ def getInterVowelRatio(_textgridFilename):
 
 	return vowelRatio
 
+def getConsecutiveVowelRatio(_textgridFilename):
+	"""
+	This function extracts the ratio between each consecutive pair of vowel
+	read from the textGrid file given in input
+	"""
+	vowelRatio = []
+	vowelDur = []
+	currUtterance = extractDur.parseTextgrid(_textgridFilename)
+	currPhoneList = currUtterance.phoneList
+	for phonei in currPhoneList:
+		if extractDur.isVowel(phonei).find('-1') != -1 or \
+		extractDur.isVowel(phonei).find('0') != -1 or \
+		phonei.end == currUtterance.end:
+			pass
+		else:
+			dur = float(phonei.end) - float(phonei.start)
+			vowelDur.append(dur)
+
+	for i in range(len(vowelDur)-1):
+		ratio = vowelDur[i+1]/vowelDur[i]
+		vowelRatio.append(ratio)
+
+	return vowelRatio
+
+
 def writeRatioText(_textFilename, _textgridList):
 	"""
 	This function writes ratio info for each file into a text file
 	"""
 	with open(_textFilename, 'w') as outfile:
 		for filei in _textgridList:
-			ratios = getInterVowelRatio(filei)
+			# ratios = getInterVowelRatio(filei)
+			ratios = getConsecutiveVowelRatio(filei)
 			lineToWrite = [filei[:-9]] + ratios # Don't write extension
 
 			for element in lineToWrite:
@@ -60,13 +86,15 @@ def writeRatioText(_textFilename, _textgridList):
 
 def main():
 	ROOT = 'C:\\Users\\QuyThao\\Documents\\Prosody analysis\\Tests_ERJ_TIMIT\\ERJ\\'
-	GRID_DIR = ROOT + 'Alignment\\mono_align_words_full\\ERJ.TextGrid\\'
-	RES_FILE_RATIO = ROOT + 'Alignment\\mono_align_words_full\\Results\\vowelratio_handseg_erj.txt'
+	GRID_DIR = ROOT + 'Native\\Alignment\\mono_ali_native_newwords\\TextGrid\\'
+	RES_FILE_RATIO = ROOT + 'Native\\Alignment\\mono_ali_native_newwords\\Results\\vowelratio_handseg_native.txt'
+	RES_FILE_RATIO_CONS = ROOT + 'Native\\Alignment\\mono_ali_native_newwords\\Results\\vowelratioconsecutive_handseg_native.txt'
 	# RES_FILE_RATIO = 'test_print.txt'
 	textgridlist = glob.glob(GRID_DIR + '*.TextGrid')
 	# textgridlist = glob.glob('*.TextGrid')
 
-	writeRatioText(RES_FILE_RATIO,textgridlist)
+	# writeRatioText(RES_FILE_RATIO,textgridlist)
+	writeRatioText(RES_FILE_RATIO_CONS,textgridlist)
 
 if __name__ == '__main__':
 	main()
